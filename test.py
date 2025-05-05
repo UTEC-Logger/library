7bb6305153de3570e65d2b3a51fe6da0087b2873
Maykol Morales Morante <maykol.morales.morante@gmail.com>
2025-05-04 21:16:13 -0500

    Final

--- test.py
+++ test.py
@@ -2,22 +2,18 @@ from utec_logger import logger
 
 
 def test_logger_info():
-    print("Testing info log...")
     logger.info("This is an info message.")
 
 
 def test_logger_warning():
-    print("Testing warning log...")
     logger.warning("This is a warning message.")
 
 
 def test_logger_error():
-    print("Testing error log...")
     logger.error("This is an error message.")
 
 
 def test_logger_critical():
-    print("Testing critical log...")
     logger.critical("This is a critical message.")
 
 
